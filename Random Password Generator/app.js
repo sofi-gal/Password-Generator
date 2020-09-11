3b777b9ee05aee8b3924c02b89cfdfe0747e7d64
sofi-gal <61889647+sofi-gal@users.noreply.github.com>
2020-09-11 15:49:32 -0300

    Add files via upload

---
+++ Random Password Generator/app.js
@@ -0,0 +1,14 @@
+function getPassword(){
+    var chars = "01234567890abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ!@#$%&/()_-+?><:{}[]"
+
+    var passwordLength = 16;
+    var password = "";
+
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber,randomNumber+1);
+    }
+    
+    document.getElementById('password').value = password;
+}
+
